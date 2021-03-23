28e8e917aee550a0e642b955677b360b3a27e7db
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-03-24 00:26:50 +0530

    returning token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator/check');
-
 const User = require('../../models/User')
+
 // @route GET api/users 
 // @desc Rgister user
 // @access Public
@@ -23,12 +27,35 @@ async (req,res) => {
         let user = await User.findOne({email});
 
         if(user) {
-            res.status(400).json({errors:[{msg:'user already exists'}]});
+           return res.status(400).json({errors:[{msg:'user already exists'}]});
         }
         //get users gravatar
+        const avatar = gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        });
+
+        user = new User({
+            name,email,avatar,password
+        });
         //encrypt pass
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
         //return token
-        res.send('User route');
+        const payload = {
+            user:{
+                id:user.id //_id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'),
+        {expiresIn:360000},
+        (err,token)=>{
+            if(err) throw err;
+            res.json({token});
+        });
         
     } catch (error) {
         console.error(err.message);
