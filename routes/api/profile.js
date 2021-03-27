dd136821babf52fd139a8c4a9b8b9a6f5924be5e
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-03-27 22:34:04 +0530

    addind delete route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -127,7 +127,7 @@ router.get('/user/:user_id', async (req,res)=>{
 // @route   DELETE api/profile
 // @desc    Delete profile,user and posts
 // @access  Private
-router.get('/',auth, async (req,res)=>{
+router.delete('/',auth, async (req,res)=>{
     try {
         await Profile.findOneAndRemove({user: req.user.id}); 
         await User.findOneAndRemove({_id: req.user.id});
