ee05f0be7431b238a5fe6d0be0e2ed8f188a821b
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-03-30 12:02:00 +0530

    Creating Post Schema

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+const PostSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref:'users'
+    },
+    text:{
+        type: String,
+        required: true
+    },
+    name:{
+        type:String
+    },
+    avatar:{
+        type:String
+    },
+    likes:[
+        {
+            user:{
+                type : Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments : [
+        {
+            user:{
+                type : Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text : {
+                type: String,
+                required:true
+            },
+            name:{
+                type:String
+            },
+            avatar:{
+                type:String
+            },
+            date:{
+                type: Date,
+                default : Date.now
+            }
+        }
+    ],
+    date:{
+                type: Date,
+                default : Date.now
+            }
+});
+
+module.exports = Post = mongoose.model('post',Post);
