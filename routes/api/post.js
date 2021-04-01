4b62eca09b8546cba0e150e45aadc123121b3790
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-04-01 22:56:28 +0530

    GET all the posts

--- routes/api/post.js
+++ routes/api/post.js
@@ -36,4 +36,23 @@ async (req,res) =>{
         res.status(500).send('Server Error');
     }
 } );
+
+
+// @route  GET api/post
+// @desc   Get all post
+// @access Private
+
+router.get('/',auth,async (req,res)=>{
+    try {
+        const posts = await Post.find().sort({date:-1});
+        res.json(posts);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
 module.exports = router;
