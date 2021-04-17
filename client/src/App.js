036466f965b24475cc6f6e7fe00e480e34618d5d
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-04-18 00:39:57 +0530

    Edit Profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React,{Fragment,useEffect} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
@@ -7,40 +7,43 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-froms/CreateProfile';
+import EditProfile from './components/profile-froms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 //redux
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 
-import{loadUser} from './actions/auth';
+import { loadUser } from './actions/auth';
 import setAuthToken from './utils/seAuthToken'
 
-if(localStorage.token){
-        setAuthToken(localStorage.token);
-    }
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-const App= () =>{
-  useEffect(()=>{
+const App = () => {
+  useEffect(() => {
     store.dispatch(loadUser());
-  },[]); // adding empty bracket to only run once not in loop
-  return(
-  <Provider store={store}>
-  <Router>
-    <Fragment>
-      <Navbar/>
-      <Route exact path='/' component={Landing}/>
-      <section className="container">
-        <Alert/>
-        <Switch>
-          <Route exact path="/register" component={Register}/>
-          <Route exact path="/login" component={Login}/>
-          <PrivateRoute exact path="/dashboard" component={Dashboard}/>
-          <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
-  </Provider>
-)};
+  }, []); // adding empty bracket to only run once not in loop
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+};
 export default App;
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,38 +1,41 @@
-import React , {Fragment, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {getCurrentProfile} from '../../actions/profile'
+import { connect } from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
 import Spinner from '../layout/spinner'
-const Dashboard = ({getCurrentProfile,
-    auth:{user},
-    profile:{profile,loading}}) => {
-useEffect(()=>{
-    getCurrentProfile();
-},[]);
+import DashboardAction from './DashboardAction'
+const Dashboard = ({ getCurrentProfile,
+    auth: { user },
+    profile: { profile, loading } }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
 
-    return loading && profile=== null ?( <Spinner/>):(<Fragment>
+    return loading && profile === null ? (<Spinner />) : (<Fragment>
         <h1 className='large text-primary'>Dashboard</h1>
         <p className="lead">
             <i className='fas fa-user'></i>Welcome {user && user.name}
         </p>
-        {profile !==null ? (
-            <Fragment>has</Fragment>
-        ):(
+        {profile !== null ? (
+            <Fragment>
+                <DashboardAction />
+            </Fragment>
+        ) : (
             <Fragment>
                 <p>You have not yet setup a profile , Add some info</p>
                 <Link to='/create-profile' className='btn btn-primary my1'>
                     Create Profile
                 </Link>
-                
+
             </Fragment>)}
-    </Fragment> );
+    </Fragment>);
 }
 
 Dashboard.propTypes = {
-getCurrentProfile: PropTypes.func.isRequired,
-auth: PropTypes.object.isRequired,
-profile:PropTypes.object.isRequired
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
@@ -40,4 +43,4 @@ const mapStateToProps = state => ({
     profile: state.profile
 });
 
-export default connect(mapStateToProps,{getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
