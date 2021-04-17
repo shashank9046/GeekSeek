69e7a760ead53c41abf7b8f214fbdfbce55101ec
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-04-18 00:39:46 +0530

    fixing GoBack button to redirect to dashboard

--- client/src/components/profile-froms/CreateProfile.js
+++ client/src/components/profile-froms/CreateProfile.js
@@ -1,53 +1,53 @@
-import React,{Fragment, useState} from 'react'
-import {Link,withRouter} from 'react-router-dom'
+import React, { Fragment, useState } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {createProfile} from '../../actions/profile'
+import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
 
-const CreateProfile = ({createProfile, history}) => {
-    const[formData,setFormData] = useState({
-        company:'',
-        website:'',
-        location:'',
-        status:'',
-        skills:'',
-        githubusername:'',
-        bio:'',
-        twitter:'',
-        facebook:'',
-        youtube:'',
-        linkedin:'',
-        instagram:''
-    });
+const CreateProfile = ({ createProfile, history }) => {
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    youtube: '',
+    linkedin: '',
+    instagram: ''
+  });
 
-const[displaySocialInputs,toggleSocialInputs]= useState(false);
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
-     const {
-        company,
-        website,
-        location,
-        status,
-        skills,
-        githubusername,
-        bio,
-        twitter,
-        facebook,
-        youtube,
-        linkedin,
-        instagram
-     }=formData;
+  const {
+    company,
+    website,
+    location,
+    status,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    facebook,
+    youtube,
+    linkedin,
+    instagram
+  } = formData;
 
-const onChange = e => setFormData({...formData,[e.target.name]: e.target.value});
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-const onSubmit = e =>{
-  e.preventDefault();
-  createProfile(formData,history);
-}
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  }
 
-    return (
-        <Fragment>
-            <h1 className="large text-primary">
+  return (
+    <Fragment>
+      <h1 className="large text-primary">
         Create Your Profile
       </h1>
       <p className="lead">
@@ -55,10 +55,10 @@ const onSubmit = e =>{
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form" onSubmit={e=>onSubmit(e)}>
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={
-              e=>onChange(e)
+            e => onChange(e)
           }>
             <option value="0">* Select Professional Status</option>
             <option value="Developer">Developer</option>
@@ -71,39 +71,39 @@ const onSubmit = e =>{
             <option value="Other">Other</option>
           </select>
           <small className="form-text"
-            >Give us an idea of where you are at in your career</small
+          >Give us an idea of where you are at in your career</small
           >
         </div>
         <div className="form-group">
           <input type="text" placeholder="Company" name="company" value={company} onChange={
-              e=>onChange(e)
+            e => onChange(e)
           } />
           <small className="form-text"
-            >Could be your own company or one you work for</small
+          >Could be your own company or one you work for</small
           >
         </div>
         <div className="form-group">
           <input type="text" placeholder="Website" name="website" value={website} onChange={
-              e=>onChange(e)
-          }/>
+            e => onChange(e)
+          } />
           <small className="form-text"
-            >Could be your own or a company website</small
+          >Could be your own or a company website</small
           >
         </div>
         <div className="form-group">
           <input type="text" placeholder="Location" name="location" value={location} onChange={
-              e=>onChange(e)
-          }/>
+            e => onChange(e)
+          } />
           <small className="form-text"
-            >City & state suggested (eg. Boston, MA)</small
+          >City & state suggested (eg. Boston, MA)</small
           >
         </div>
         <div className="form-group">
           <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={
-              e=>onChange(e)
-          }/>
+            e => onChange(e)
+          } />
           <small className="form-text"
-            >Please use comma separated values (eg.
+          >Please use comma separated values (eg.
             HTML,CSS,JavaScript,PHP)</small
           >
         </div>
@@ -113,73 +113,73 @@ const onSubmit = e =>{
             placeholder="Github Username"
             name="githubusername"
             value={githubusername} onChange={
-              e=>onChange(e)
-          }
+              e => onChange(e)
+            }
           />
           <small className="form-text"
-            >If you want your latest repos and a Github link, include your
+          >If you want your latest repos and a Github link, include your
             username</small
           >
         </div>
         <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"value={bio} onChange={
-              e=>onChange(e)
+          <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={
+            e => onChange(e)
           }></textarea>
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
         <div className="my-2">
-          <button onClick={()=>toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
+          <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
-          {displaySocialInputs && <Fragment>
-              <div className="form-group social-input">
-          <i className="fab fa-twitter fa-2x"></i>
-          <input type="text" placeholder="Twitter URL" name="twitter"value={twitter} onChange={
-              e=>onChange(e)
-          } />
-        </div>
+        {displaySocialInputs && <Fragment>
+          <div className="form-group social-input">
+            <i className="fab fa-twitter fa-2x"></i>
+            <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={
+              e => onChange(e)
+            } />
+          </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-facebook fa-2x"></i>
-          <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={
-              e=>onChange(e)
-          } />
-        </div>
+          <div className="form-group social-input">
+            <i className="fab fa-facebook fa-2x"></i>
+            <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={
+              e => onChange(e)
+            } />
+          </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-youtube fa-2x"></i>
-          <input type="text" placeholder="YouTube URL" name="youtube" value={youtube} onChange={
-              e=>onChange(e)
-          }/>
-        </div>
+          <div className="form-group social-input">
+            <i className="fab fa-youtube fa-2x"></i>
+            <input type="text" placeholder="YouTube URL" name="youtube" value={youtube} onChange={
+              e => onChange(e)
+            } />
+          </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-linkedin fa-2x"></i>
-          <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={
-              e=>onChange(e)
-          }/>
-        </div>
+          <div className="form-group social-input">
+            <i className="fab fa-linkedin fa-2x"></i>
+            <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={
+              e => onChange(e)
+            } />
+          </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-instagram fa-2x"></i>
-          <input type="text" placeholder="Instagram URL" name="instagram"value={instagram} onChange={
-              e=>onChange(e)
-          } />
-        </div>
-              </Fragment>}          
-        
+          <div className="form-group social-input">
+            <i className="fab fa-instagram fa-2x"></i>
+            <input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={
+              e => onChange(e)
+            } />
+          </div>
+        </Fragment>}
+
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
+        <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
       </form>
-        </Fragment>
-    )
+    </Fragment>
+  )
 }
 
 CreateProfile.propTypes = {
-  createProfile:PropTypes.func.isRequired
+  createProfile: PropTypes.func.isRequired
 };
 
-export default connect (null,{createProfile})(withRouter(CreateProfile));
+export default connect(null, { createProfile })(withRouter(CreateProfile));
