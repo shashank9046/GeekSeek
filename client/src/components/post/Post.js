36fd93d106a15bf895d38431a7fb3f07135e5e99
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2021-04-20 14:19:38 +0530

    Deleting comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
 import { Link } from 'react-router-dom'
 import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
@@ -15,6 +16,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     return loading || post === null ? <Spinner /> : <Fragment>
         <PostItem post={post} showActions={false} />
         <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
 }
 
